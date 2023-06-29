a75136470ff2b83713f9bce1be5f34d22dab707a
francaicey <francawhyte447@gmail.com>
2023-06-29 06:31:39 -0700

    all new files

--- ptr_prompt.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-/**
- * print_prompt - print prompt
- */
-void print_prompt(void)
-{
-	 printf("$ ");
-	 fflush(stdout);
-}
--- string_utils.c
+++ string_utils.c
@@ -1,105 +1,84 @@
 #include "main.h"
-
 /**
- * _strcat - concatenates two strings.
- * @dest: The destination string.
- * @src: The source string.
- *
- * Return: A pointer to the resulting string.
+ * _memcpy - Copies a block of memory from the source
+ * pointer to the destination pointer
+ * @new_pointer: The destination pointer.
+ * @pointer: The source pointer
+ * @s: The size of the new pointer.
+ * Return: None.
  */
-char *_strcat(char *dest, const char *src)
+void _memcpy(void *new_pointer, const void *pointer, unsigned int s)
 {
-	int dest_len = _strlen(dest);
-	int i;
+	char *char_ptr = (char *)pointer;
+	char *char_newptr = (char *)new_pointer;
+	unsigned int i;
 
-	for (i = 0; src[i] != '\0'; i++)
-		dest[dest_len++] = src[i];
-
-	dest[dest_len] = '\0';
-
-	return (dest);
+	for (i = 0; i < s; i++)
+		char_newptr[i] = char_ptr[i];
 }
 
 /**
- * _strcmp - compares two strings.
- * @s1: The first string.
- * @s2: The second string.
- *
- * Return: An integer less than, equal to, or greater than zero
- *         if s1 is less than, equal to, or greater than s2, respectively.
+ * _realloc - Reallocates a memory block with a new size.
+ * @pointer: Pointer to the memory previously allocated
+ * @old_s: Size, in bytes, of the allocated space for ptr.
+ * @new_s: New size, in bytes, of the reallocated memory block
+ * Return: Pointer to the reallocated memory block (ptr).
  */
-int _strcmp(const char *s1, const char *s2)
+void *_realloc(void *pointer, unsigned int old_s, unsigned int new_s)
 {
-	int i;
+	void *newptr;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	if (pointer == NULL)
+		return (malloc(new_s));
+
+	if (new_s == 0)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		free(pointer);
+		return (NULL);
 	}
 
-	return (s1[i] - s2[i]);
+	if (new_s == old_s)
+		return (pointer);
+
+	newptr = malloc(new_s);
+	if (newptr == NULL)
+		return (NULL);
+
+	if (new_s < old_s)
+		_memcpy(newptr, pointer, new_s);
+	else
+		_memcpy(newptr, pointer, old_s);
+
+	free(pointer);
+	return (newptr);
 }
 
 /**
- * _strlen - calculates the length of a string.
- * @s: The input string.
- *
- * Return: The length of the string.
+ * _reallocdp - Reallocates a memory block of a double pointer.
+ * @pointer: Double pointer to the memory previously allocated
+ * @old_s: Size, in bytes, of the allocated space for pointer.
+ * @new_s: New size, in bytes, of the reallocated memory block
+ * Return: Pointer to the reallocated memory block (pointer).
  */
-int _strlen(const char *s)
+char **_reallocdp(char **pointer, unsigned int old_s, unsigned int new_s)
 {
-	int len = 0;
+	char **newptr;
+	unsigned int i;
 
-	while (s[len] != '\0')
-		len++;
+	if (pointer == NULL)
+		return (malloc(sizeof(char *) * new_s));
 
-	return (len);
+	if (new_s == old_s)
+		return (pointer);
+
+	newptr = malloc(sizeof(char *) * new_s);
+	if (newptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_s; i++)
+		newptr[i] = pointer[i];
+
+	free(pointer);
+
+	return (newptr);
 }
-
-/**
- * _strncmp - compares two strings up to a given length.
- * @s1: The first string.
- * @s2: The second string.
- * @n: The maximum number of characters to compare.
- *
- * Return: An integer less than, equal to, or greater than zero
- *         if s1 is less than, equal to, or greater than s2, respectively.
- */
-int _strncmp(const char *s1, const char *s2, size_t n)
-{
-	size_t i;
-
-	for (i = 0; i < n && s1[i] != '\0' && s2[i] != '\0'; i++)
-	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-	}
-
-	if (i == n)
-		return (0);
-
-	return (s1[i] - s2[i]);
-}
-
-/**
- * _strcpy - copies a string.
- * @dest: The destination string.
- * @src: The source string.
- *
- * Return: A pointer to the destination string.
- */
-char *_strcpy(char *dest, const char *src)
-{
-	int i = 0;
-
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-
-	return (dest);
-}
-
